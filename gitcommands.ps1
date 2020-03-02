0d0d5c5bb32ab38d5f8ea82cbc0b174522562c6f
Nouamane Tazi <nouamane98@gmail.com>
2020-03-02 11:08:51 +0100

    Update gitcommands.ps1

--- gitcommands.ps1
+++ gitcommands.ps1
@@ -1,1 +1,7 @@
 $ git fetch -p # Delete multiple obsolete tracking branches
+
+// delete branch locally
+git branch -d localBranchName
+
+// delete branch remotely
+git push origin --delete remoteBranchName
